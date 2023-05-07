78614be822bdf7a6e3689ace2a98c212cb628fd3
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-05-07 14:34:07 +0800

    mark app and reset app

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-const bac = document.querySelector('.red')
-bac.textContent = 'hjo'
+
+// gameBoard
+const gameBoard = (() => {
+  const player1mark = 'Ｏ'
+  const player2mark = '✘'
+  // order
+
+  let playOrder = ['Ｏ', '✘', 'Ｏ', '✘', 'Ｏ', '✘', 'Ｏ', '✘', 'Ｏ']
+  const boardHtml = document.querySelectorAll('.mark')
+
+  const markAction = () => {
+    boardHtml.forEach((block) => {
+      block.addEventListener('click', (e) => {
+        if (block.innerHTML === '') {
+          block.innerHTML = playOrder[0]
+          playOrder.shift()
+        }
+        console.log(playOrder)
+      })
+    })
+  }
+  const reset = () => {
+    playOrder = ['Ｏ', '✘', 'Ｏ', '✘', 'Ｏ', '✘', 'Ｏ', '✘', 'Ｏ']
+    boardHtml.forEach((block) => {
+      block.innerHTML = ''
+    })
+  }
+  return {
+    markAction,
+    reset
+  }
+})()
